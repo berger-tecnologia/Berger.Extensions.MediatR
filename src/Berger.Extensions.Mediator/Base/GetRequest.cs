103bf06207be539c1e193c1d5bcf0edd84f335df
Rafael Cronemberger <rafael@berger.com.br>
2023-11-27 03:05:45 -0300

    Package updates

---
+++ src/Berger.Extensions.Mediator/Base/GetRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Berger.Extensions.Abstractions;
+
+namespace Berger.Extensions.Mediator
+{
+    public class GetRequest<T> where T : BaseEntity
+    {
+        public record GetById(Guid Id) : IRequest<T>;
+        public record Get() : IRequest<IQueryable<T>>;
+    }
+}
